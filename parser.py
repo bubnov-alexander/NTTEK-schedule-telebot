52ca7fb11a9974000fbeffa85561594d0452e3e2
Kinoki445 <Kinoki445@gmail.com>
2022-12-04 01:52:21 +0500

    bot6.0

--- parser.py
+++ parser.py
@@ -4,40 +4,48 @@ import datetime as dt
 
 def getpari(date, group, group_name, InlineKeyboardMarkup, InlineKeyboardButton, bot, callback):
     if group != 'teacher':
-        site = requests.get(f'https://erp.nttek.ru/api/schedule/legacy').text
-        sitedate = json.loads(site)
-        sitedate.sort(key=lambda x: time.mktime(time.strptime(x,"%d.%m.%Y")))
-
-        if (len(sitedate)) <= 5:
-            a = 0
-        else:
-            a = ((len(sitedate)) - 5)
-
-        date = date.replace('.','-')
-        now = dt.datetime.strptime(date, '%d-%m-%Y')
-        now = now.strftime('%Y-%m-%d')
-        r = requests.get(f'https://erp.nttek.ru/api/schedule/legacy/{now}/{group}/{group_name}').text
-        data = json.loads(r)
         try:
-            text = ''
-            for i in data['schedule']:
-                lesson = i['lesson']
-                name = i['name']
-                room = i['rooms']
-                teacher = i['teachers']
-                text = (text + '\n' + (f'**Номер пары:** {lesson}\n**Пара:** {name} {room}\n**Препод:** {teacher}\n'))
-        
-            keyboard = InlineKeyboardMarkup()
-            keyboard.row_width = 2
-            for i in range(a, len(sitedate)):
-                keyboard.add (InlineKeyboardButton(f'{sitedate[i]} {group_name}',callback_data = f'{sitedate[i]} {group_name}'))
-            bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text = f'Расписание на {date}:\n __{text}__ \nВыберите день на который хотите узнать расписание', parse_mode='Markdown', reply_markup = keyboard)
+            site = requests.get(f'https://erp.nttek.ru/api/schedule/legacy').text
+            sitedate = json.loads(site)
+            sitedate.sort(key=lambda x: time.mktime(time.strptime(x,"%d.%m.%Y")))
+
+            if (len(sitedate)) <= 5:
+                a = 0
+            else:
+                a = ((len(sitedate)) - 5)
+
+            date = date.replace('.','-')
+            now = dt.datetime.strptime(date, '%d-%m-%Y')
+            now = now.strftime('%Y-%m-%d')
+            r = requests.get(f'https://erp.nttek.ru/api/schedule/legacy/{now}/{group}/{group_name}').text
+            data = json.loads(r)
+            try:
+                text = ''
+                for i in data['schedule']:
+                    lesson = i['lesson']
+                    name = i['name']
+                    room = i['rooms']
+                    teacher = i['teachers']
+                    text = (text + '\n' + (f'**Номер пары:** {lesson}\n**Пара:** {name} {room}\n**Препод:** {teacher}\n'))
+            
+                keyboard = InlineKeyboardMarkup()
+                keyboard.row_width = 2
+                for i in range(a, len(sitedate)):
+                    keyboard.add (InlineKeyboardButton(f'{sitedate[i]} {group_name}',callback_data = f'{sitedate[i]} {group_name}'))
+                keyboard.add(InlineKeyboardButton('Другие группы', callback_data = 'another_group'))
+                bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text = f'Расписание на {date}:\n __{text}__ \nВыберите день на который хотите узнать расписание', parse_mode='Markdown', reply_markup = keyboard)
+            except:
+                keyboard = InlineKeyboardMarkup()
+                keyboard.row_width = 2
+                for i in range(a, len(sitedate)):
+                    keyboard.add (InlineKeyboardButton(f'{sitedate[i]} {group_name}',callback_data = f'{sitedate[i]} {group_name}'))
+                keyboard.add(InlineKeyboardButton('Другие группы', callback_data = 'another_group'))
+                bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text = f'Расписания НЕТУ!\nВыберите день на который хотите узнать расписание', parse_mode='Markdown', reply_markup = keyboard)
         except:
             keyboard = InlineKeyboardMarkup()
             keyboard.row_width = 2
-            for i in range(a, len(sitedate)):
-                keyboard.add (InlineKeyboardButton(f'{sitedate[i]} {group_name}',callback_data = f'{sitedate[i]} {group_name}'))
-            bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text = f'Расписания НЕТУ!\nВыберите день на который хотите узнать расписание', parse_mode='Markdown', reply_markup = keyboard)
+            keyboard.add(InlineKeyboardButton('Другие группы', callback_data = 'another_group'))
+            bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text = f'Такой группы нету выбери другую', parse_mode='Markdown', reply_markup = keyboard)
     else:
         site = requests.get(f'https://erp.nttek.ru/api/schedule/legacy').text
         sitedate = json.loads(site)
